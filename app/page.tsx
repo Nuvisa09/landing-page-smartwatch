48266270ea9564fb38b4ac56e91adb4805ad936f
Nuvisa09 <nurnisa0922@gmail.com>
2025-07-26 18:16:25 +0700

    ubah posisi folder lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,36 @@
 import FeatureCard from "./components/FeatureCard";
 import Image from "next/image";
-import { BatteryIcon, BellIcon, HeartIcon } from "./lib/icon";
+import { BatteryIcon, BellIcon, HeartIcon } from "../lib/icon";
 
 export default function SmartWatchPage() {
   return (
-    <div className="container mx-auto px-4 py-13 n">
-      <section className="flex flex-col lg:flex-row items-center justify-between text-center lg:text-left py-12 px-8 lg:mt-17 ">
+    <div className="container mx-auto px-4 py-14 n">
+      <section className="flex flex-col lg:flex-row items-center justify-between text-center lg:text-left py-12 px-8 lg:mt-20 ">
         <div className="order-1 lg:order-2 lg:mt-0 lg:w-1/2 flex justify-center lg:w-[600px] mx-auto mb-8">
           <Image
             src="/watch2.png"
             alt="Smartwatch product image"
             width={400}
             height={400}
-            className="w-full h-auto"
+            className="h-auto"
           />
         </div>
         <div className="order-2 lg:order-1 text-center lg:text-right lg:w-1/2">
           <h1 className="text-4xl md:text-6xl font-extrabold text-[#EFE4D2]  tracking-tight lg:mb-10">
             The Future on Your Wrist
           </h1>
-          <p className="mt-4 mx-w-xl mx-auto text-lg text-slate-400 lg:ml-20">
+          <p className="mt-4 max-w-xl mx-auto text-lg text-slate-400 lg:ml-20">
             Experience seamless connectivity and track your health like ever
             before. Elegance meets technology.
           </p>
-          <div className="mt-8 text-center lg:text-left lg:ml-30">
+          <div className="mt-8 text-center lg:text-left lg:ml-32">
             <button className="mt-8 bg-[#EFE4D2] text-black px-8 py-8 rounded-full font-semibold hover:bg-[#FFCB61] hover:text-black transition-colors ">
               Buy Now
             </button>
           </div>
         </div>
       </section>
-      <section className="my-30 ">
+      <section className="my-32 ">
         <h2 className="text-3xl font-bold text-center mb-20 text-[#EFE4D2] lg:text-5xl ">
           Key Features SmartWatch
         </h2>
